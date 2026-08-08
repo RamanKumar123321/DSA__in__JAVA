56e2d3cb2568673f6c34e6b0ac3b88f1b9775195
RamanKumar123321 <suman9572733788@gmail.com>
2026-08-08 22:57:56 +0530

    this is my first java programme for input and output.

---
+++ Basic/CountDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CountDigits{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int a=sc.nextInt();
+        int count=0;
+        while(a>0){
+            a=a/10;
+            count++;
+        }
+        System.out.println("Number of digits: " +count);
+    }
+
+}
---
+++ Basic/PrimeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int a=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+            }
+        if(count==2){
+            System.out.println("The given number is a Prime Number");
+        }else{
+            System.out.println("The given number is not a Prime Number");
+        }
+        }
+
+    }
