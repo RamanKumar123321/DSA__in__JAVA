bef47b756d7b8f2c276f70bf65abb15cb305e0cf
RamanKumar123321 <suman9572733788@gmail.com>
2026-09-16 22:09:51 +0530

    this is my new experience in pattern printing;

---
+++ Binary_ Search method/Longest_Substring_without_Repeatation.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class Longest_Substring_without_Repeatation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        System.out.println("Longest length = " + max);
+    }
+}
