8fa30a71c5b72e158c874e79afcf5f7aa828e1c4
RamanKumar123321 <suman9572733788@gmail.com>
2026-09-19 23:29:57 +0530

    this is my new experience in pattern printing;

---
+++ Binary_ Search method/Basic_Binary_Search_Method.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Basic_Binary_Search_Method {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int s=0,e=0;
+        int mid=0;
+    }
+}
